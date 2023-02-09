8d432907501e98355a03f451f5a7823658edbd20
samir-anan <samir.anan.dev@gmail.com>
2023-02-09 12:32:08 +0200

    04.1 authentication with breeze s

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,16 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\View;
 
 class DashboardController extends Controller
 {
+
+    public function __construct(){
+        // $this->middleware(['auth']);
+        // $this->middleware(['auth'])->except(['index']);
+        // $this->middleware(['auth'])->only(['index']);
+    }
+
     public function index()
     {
-        return view('dashboard.index')->with([
-            'name' => 'Mohammed Safadi'
-        ]);
+        return view('dashboard.index');
 //        return View::make('dashboard')->with([
 //            'name' => 'Mohammed Safadi'
 //        ]);
