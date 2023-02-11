e5f6dcd6602f2e7488c7f572e90075264d32c22a
samir-anan <samir.anan.dev@gmail.com>
2023-02-11 13:58:03 +0200

    route grouping

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -2,9 +2,29 @@
 
 
 use App\Http\Controllers\Dashboard\CategoriesController;
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Dashboard\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/dashboard',[DashboardController::class,'index']);
-Route::resource('dashboard/categories', CategoriesController::class);
+/*
+ * Route::group([
+    'middleware' => ['auth'],
+    'prefix'     => 'dashboard',
+    'as'         => 'dashboard.',
+   // 'namespace' => 'App\Http\Controllers\Dashboard',
+],function (){
+    // Route::get('/','DashboardController@index')
+     ->only('edit','show')
+     ->except('index','create');
+    Route::resource('/', DashboardController::class);
+    Route::resource('/categories', CategoriesController::class);
+});
+*/
+
+Route::middleware(['auth'])
+    ->as('dashboard.')
+    ->prefix('dashboard')
+    ->group(function (){
+        Route::resource('/', DashboardController::class);
+        Route::resource('/categories', CategoriesController::class);
+    });
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Dashboard\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -35,3 +35,4 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
+require __DIR__ . '/dashboard.php';
