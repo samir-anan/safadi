d6f41afe55829ade579f95fc9cc9307a01079a73
samir-anan <samir.anan.dev@gmail.com>
2023-02-16 12:04:39 +0200

    07.1 pagination and query search

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
@@ -32,5 +33,8 @@ class AppServiceProvider extends ServiceProvider
             return !(in_array(strtolower($value),$params));
         },
         'incorrect value');
+
+        Paginator::useBootstrap();
+       // Paginator::defaultView('pagination.name'); for custom pagination
     }
 }
